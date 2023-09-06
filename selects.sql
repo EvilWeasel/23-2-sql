60b9c85348ae48abce4b107d37dd443eb611921d
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2023-09-06 08:33:24 +0200

    Ende erster Tag

---
+++ selects.sql
@@ -0,0 +1,19 @@
+SELECT * FROM videogames;
+
+SELECT Id, Title, Releasedate, Genre FROM videogames;
+
+SELECT Count(Id) FROM videogames;
+
+SELECT Id from videogames;
+
+
+SELECT * FROM developer;
+
+
+SELECT Id, Title, developerId
+FROM videogames
+WHERE DeveloperId=92;
+
+SELECT Count(Id)
+FROM videogames
+WHERE DeveloperId=92;
